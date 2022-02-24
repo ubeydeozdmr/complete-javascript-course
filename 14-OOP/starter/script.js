fcbfd952dfcbecec964154519fc8f0129da88abb
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-02-24 22:45:14 +0300

    Static Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const date = new Date();
 /*
+const date = new Date();
+
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -23,12 +24,14 @@ console.log(jonas);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
-
-const jay = 'Jay';
 
 console.log(jonas instanceof Person);
-console.log(jay instanceof Person);
+
+Person.hey = function () {
+  console.log('Hey there 👋');
+  console.log(this);
+};
+Person.hey();
 
 // Prototypes
 console.log(Person.prototype);
@@ -98,7 +101,6 @@ bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
-*/
 
 // class expression
 // const PersonCl = class {};
@@ -110,6 +112,7 @@ class PersonCl {
     this.birthYear = birthYear;
   }
 
+  // Instance methods
   // Methods will be added to .prototype property
   calcAge() {
     console.log(date.getFullYear() - this.birthYear);
@@ -133,6 +136,12 @@ class PersonCl {
   get fullName() {
     return this._fullName;
   }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
 }
 
 const jessica = new PersonCl('Jessica Davis', 1996);
@@ -153,6 +162,8 @@ jessica.greet();
 
 const walter = new PersonCl('Walter White', 1965);
 
+PersonCl.hey();
+
 const account = {
   owner: 'Jonas',
   movements: [200, 530, 120, 300],
@@ -170,3 +181,4 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+*/
