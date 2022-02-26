8bcdb28d3e566dd75a52e3d9fdc51ab59aa83f47
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-02-26 13:46:04 +0300

    Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,3 +1,30 @@
 'use strict';
 
 const date = new Date();
+
+// ! //////////////////////////////////////////
+// ! Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(date.getFullYear() - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// ? 1. New {} is created
+// ? 2. function is called, this = {}
+// ? 3. linked to prototype
+// ? 4. function automaricly return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
