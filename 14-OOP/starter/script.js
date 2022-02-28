61b42e82164271d3212a873a2981b05e1046f2b2
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-02-28 22:43:56 +0300

    Coding Challenge #4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -405,7 +405,7 @@ class PersonCl {
 // ! //////////////////////////////////////////
 // ! Inheritance Between "Classes": ES6 Classes
 
-class StudentCl extends PersonCl {
+class StudentCl extends PersonCl { // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   constructor(fullName, birthYear, course) {
     // ? Always needs to happen first!
     super(fullName, birthYear);
@@ -461,7 +461,6 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
-*/
 
 // ! //////////////////////////////////////////
 // ! Another Class Example
@@ -548,3 +547,69 @@ Account.helper();
 // ? Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+*/
+
+// * //////////////////////////////////////////
+// * Coding Challenge #4
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`${this.make} charged to ${chargeTo}%`);
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.charge);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
