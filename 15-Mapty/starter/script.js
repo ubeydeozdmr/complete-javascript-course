463c6855e770e38ea6e1f48795a1bb1650f3b9bb
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-02 17:06:25 +0300

    Working with localStorage

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -61,9 +61,9 @@ class Cycling extends Workout {
   }
 }
 
-const run1 = new Running([39, -12], 5.2, 24, 178);
-const cycling1 = new Cycling([39, -12], 27, 95, 523);
-console.log(run1, cycling1);
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
 
 // ! /////////////////////////////////
 // ! APPLICATION ARCHITECTURE
@@ -83,7 +83,13 @@ class App {
   #workouts = [];
 
   constructor() {
+    // ? Get user's position
     this._getPosition();
+
+    // ? Get data from local storage
+    this._getLocalStorage();
+
+    // ? Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -106,7 +112,6 @@ class App {
 
     const coords = [latitude, longitude];
 
-    console.log(this);
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
     //   console.log(map);
 
@@ -117,6 +122,11 @@ class App {
 
     // ? Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -186,7 +196,6 @@ class App {
 
     // ? Add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout);
 
     // ? Render workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -196,6 +205,9 @@ class App {
 
     // ? Hide form + clear input fields
     this._hideForm();
+
+    // ? Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -272,7 +284,6 @@ class App {
     const workout = this.#workouts.find(
       work => work.id === workoutEl.dataset.id
     );
-    console.log(workout);
 
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
@@ -282,7 +293,28 @@ class App {
     });
 
     // ? using the public interface
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
