bb988fa4ae62ececc9ea0e27a24188f8f28e4708
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-06 14:41:30 +0300

    The Module Pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -11,7 +11,7 @@ console.log('Importing module');
 // console.log(shoppingCart.totalPrice);
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-import add, { cart } from './shoppingCart.js';
+import add, { cart, totalPrice } from './shoppingCart.js';
 add('pizza', 2);
 add('bread', 5);
 add('apple', 4);
@@ -39,3 +39,28 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return { addToCart, cart, totalPrice, totalQuantity };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
