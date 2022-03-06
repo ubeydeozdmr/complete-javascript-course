d2e47defd2e43cf0a8d4f8868a0681e7e128242f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-06 16:29:50 +0300

    Introduction to NPM

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -77,3 +77,20 @@ console.log(shoppingCart2.shippingCost);
 
 // ? Import
 // const {addToCart} = require("./shoppingCart.js")
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
