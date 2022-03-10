a5a0a3960fc493a551c124f7bec43f19a574aa04
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-11 00:50:41 +0300

    Implementing Bookmarks - Part 1

--- 18-forkify/starter/src/js/views/View.js
+++ 18-forkify/starter/src/js/views/View.js
@@ -23,14 +23,14 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
-      console.log(curEl, newEl.isEqualNode(curEl));
+      // console.log(curEl, newEl.isEqualNode(curEl));
 
       // ? Updates changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       ) {
-        console.log('🎆', newEl.firstChild.nodeValue.trim());
+        // console.log('🎆', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
 
--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -28,6 +28,14 @@ class RecipeView extends View {
     });
   }
 
+  addHandlerAddBookmark(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--bookmark');
+      if (!btn) return;
+      handler();
+    });
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
@@ -128,9 +136,11 @@ class RecipeView extends View {
 
         <div class="recipe__user-generated">
         </div>
-        <button class="btn--round">
+        <button class="btn--round btn--bookmark">
           <svg class="">
-            <use href="${icons}#icon-bookmark-fill"></use>
+            <use href="${icons}#icon-bookmark${
+      this._data.bookmarked ? '-fill' : ''
+    }"></use>
           </svg>
         </button>
       </div>
