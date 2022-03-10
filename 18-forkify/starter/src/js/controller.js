261c88f2959e33b50d51e378286d1f3d561924b1
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-10 20:20:50 +0300

    Developing a DOM Updating Algorithm

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -19,6 +19,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // ? 0) Update results view to mark selected search result
+    resultsView.render(model.getSearchResultsPage());
+
     // ? 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -64,7 +67,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // ? Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
