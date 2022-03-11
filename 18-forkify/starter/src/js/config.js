7b2c50dd36e433f5b90d9eebc79bee96f0c79ea9
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-03-11 12:00:39 +0300

    Uploading a New Recipe - Part 2

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '0a70a3c6-1d4d-4fd3-85e1-92966ed8908e';
+export const MODAL_CLOSE_SEC = 2.5;
